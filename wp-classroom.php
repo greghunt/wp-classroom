deb1c80cd22fa68763c8d70898b3f57b3b266afd
Greg Hunt <greg@w3visi.com>
2017-05-03 18:00:07 -0400

    Increased version

--- wp-classroom.php
+++ wp-classroom.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Classroom
  * Plugin URI:        https://github.com/greghunt/wp-classroom
  * Description:       Create a multimedia classroom within your WordPress site.
- * Version:           1.1.13
+ * Version:           1.1.14
  * Author:            Fresh Brewed Web
  * Author URI:        https://freshbrewedweb.com
  * License:           GPL-2.0+
